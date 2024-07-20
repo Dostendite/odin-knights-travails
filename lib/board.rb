8cb53817cc83daf46da64b5c70bce8423d2cfd2c
Dostendite <dostendite@outlook.es>
2024-07-20 15:22:35 +0200

    Add methods to calculate possible moves
    The moves are discarded if they are out out
    bounds.

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,53 @@
 require_relative "square"
+require_relative "knight"
 
 class Board
-  attr_reader :squares
+  attr_reader :knight, :squares, :moves
 
-  def initialize
+  def initialize(knight_x = 3, knight_y = 3)
+    @knight = Knight.new("Sleipnir", knight_x, knight_y)
     @squares = generate_board
+    @moves = generate_moves
+    # @adjacency_list = generate_adjacency_list
   end
 
-  def valid_square?(square)
-    x = square.x
-    y = square.y
+  def valid_square?(square_x, square_y)
+    return false if square_x.negative? || square_y.negative?
+    return false if square_x > 7 || square_y > 7
 
-    !((x || y).negative? || (x || y) > 7)
+    true
+    # !((square_x.negative? || square_y.negative?) || (square_x || square_y) > 7)
+  end
+
+  def generate_moves
+    recipe = [1, 2, -1, -2]
+    moves = []
+
+    recipe.each do |num|
+      recipe.each do |ber|
+        next if num.abs == ber.abs
+
+        moves << [num, ber]
+      end
+    end
+    moves
+  end
+
+  def matrix_sum(square_x, square_y, move_x, move_y)
+    result_x = square_x + move_x
+    result_y = square_y + move_y
+
+    [result_x, result_y]
+  end
+
+  def check_possible_moves(square)
+    possible_moves = []
+
+    @moves.each do |move|
+      potential_move = matrix_sum(square.x, square.y, move[0], move[1])
+      possible_moves << potential_move if valid_square?(potential_move[0], potential_move[1])
+    end
+    possible_moves
   end
 
   def generate_board
